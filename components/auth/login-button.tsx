e15ff061db6bd881bd47e590bc0cf25a9af9e4e5
TreNgheDiCode <gabayan170@gmail.com>
2024-01-10 20:26:44 +0700

    Card Wrapper

--- components/auth/login-button.tsx
+++ components/auth/login-button.tsx
@@ -1,32 +1,27 @@
-"use client"
+"use client";
 
 import { useRouter } from "next/navigation";
 
-
 interface LoginButtonProps {
   children: React.ReactNode;
-  mode?: "modal" | "redirect"
+  mode?: "modal" | "redirect";
   asChild?: boolean;
 }
 
-export const LoginButton = ({children, mode, asChild}: LoginButtonProps) => {
+export const LoginButton = ({ children, mode, asChild }: LoginButtonProps) => {
   const router = useRouter();
 
   const onClick = () => {
     router.push("/auth/login");
-  }
+  };
 
   if (mode === "modal") {
-    return (
-      <span>
-        TODO: Implement Modal
-      </span>
-    )
+    return <span>TODO: Implement Modal</span>;
   }
 
   return (
     <span onClick={onClick} className="cursor-pointer">
       {children}
     </span>
-  )
-}
+  );
+};
