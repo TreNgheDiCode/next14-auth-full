4d6e126d6898285e41710ec32b77c971e653915c
TreNgheDiCode <gabayan170@gmail.com>
2024-01-15 12:21:21 +0700

    Password Reset Token

--- schemas/index.ts
+++ schemas/index.ts
@@ -1,5 +1,14 @@
 import * as z from "zod";
 
+export const ResetSchema = z.object({
+  email: z
+    .string()
+    .min(1, {
+      message: "Email is required",
+    })
+    .email(),
+});
+
 export const LoginSchema = z.object({
   email: z
     .string()
